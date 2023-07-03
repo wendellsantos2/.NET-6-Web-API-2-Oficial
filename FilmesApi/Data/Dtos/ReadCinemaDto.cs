e5a848104120ba2bd7b5698e964af26e420755fb
wendellsantos2 <wemdellcos@gmail.com>
2023-07-03 19:59:10 -0300

    project initial

---
+++ FilmesApi/Data/Dtos/ReadCinemaDto.cs
@@ -0,0 +1,15 @@
+﻿ 
+namespace FilmesApi.Data.Dtos
+{
+    public class ReadCinemaDto
+    {
+     
+        public int Id { get; set; }
+      
+
+        public string Nome { get; set; }
+
+        public ReadCinemaDto ReadEnderecoDto { get; set; }
+        public object Endereco { get; internal set; }
+    }
+}
---
+++ FilmesApi/Data/Dtos/UpdateEnderecoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmesApi.Data.Dtos
+{
+    public class UpdateEnderecoDto
+    {
+       
+
+        public string Logradouro { get; set; }
+
+        public int Numero { get; set; }
+    }
+}
